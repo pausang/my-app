59d40fe0ebf70df79a00aa4b902416104cb2fc00
pausang <kokosangpi@gmail.com>
2019-04-27 12:46:58 -0500

    Updates

---
+++ precache-manifest.767cac8bd05625d3c2f8200baf88d210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd4922ef9cec2121fd5bad797923683",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "afb99f5449592d8cf935",
+    "url": "/my-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "afb99f5449592d8cf935",
+    "url": "/my-app/static/js/main.41e591c4.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
